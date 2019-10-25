6a83852ed00cc44b2ef42438babc61f0831094e6
Geoff Goodman <ggoodman@gmail.com>
2019-10-25 16:25:49 -0400

    feat(playground): inmem cache and offline support

--- packages/playground/src/lib/ResolverHostWithCache.ts
+++ packages/playground/src/lib/ResolverHostWithCache.ts
@@ -4,6 +4,8 @@ import { openDB } from 'idb';
 import { timeout } from 'ts-primitives';
 import { TimeoutError } from './error';
 
+type ThenArg<T> = T extends Promise<infer U> ? U : T;
+
 export class ResolverHostWithCache extends AbstractResolverHost {
   private readonly idbPromise = openDB('velcro', Bundler.schemaVersion, {
     async upgrade(db, oldVersion, newVersion, transaction) {
@@ -35,6 +37,11 @@ export class ResolverHostWithCache extends AbstractResolverHost {
   private readonly inflightListEntries = new Map<string, ReturnType<ResolverHost['listEntries']>>();
   private readonly inflightReadFileContent = new Map<string, ReturnType<ResolverHost['readFileContent']>>();
 
+  private readonly getCanonicalUrlCache = new Map<string, ThenArg<ReturnType<ResolverHost['getCanonicalUrl']>>>();
+  private readonly getResolveRootCache = new Map<string, ThenArg<ReturnType<ResolverHost['getResolveRoot']>>>();
+  private readonly listEntriesCache = new Map<string, ThenArg<ReturnType<ResolverHost['listEntries']>>>();
+  private readonly readFileContentCache = new Map<string, ThenArg<ReturnType<ResolverHost['readFileContent']>>>();
+
   constructor(readonly host: ResolverHost, { idbTimeout = 1000, hostTimeout = 10000 } = {}) {
     super();
 
@@ -45,11 +52,16 @@ export class ResolverHostWithCache extends AbstractResolverHost {
   private async withCache<T, C = unknown>(
     href: string,
     loadFn: () => Promise<T>,
+    cache: Map<string, T>,
     inflightMap: Map<string, Promise<T>>,
     storeName: string,
     serialize?: (result: T) => C,
     deserialize?: (cached: C) => T
   ): Promise<T> {
+    if (cache.has(href)) {
+      return cache.get(href)!;
+    }
+
     const idb = await withTimeout(this.idbTimeout, this.idbPromise, `this.idbPromise`).catch(_ => undefined);
 
     if (idb) {
@@ -60,7 +72,11 @@ export class ResolverHostWithCache extends AbstractResolverHost {
       ).catch(_ => undefined);
 
       if (cached) {
-        return deserialize ? deserialize(cached) : cached;
+        const result = (deserialize ? deserialize(cached as any) : cached) as T;
+
+        cache.set(href, result);
+
+        return result;
       }
     }
 
@@ -73,6 +89,8 @@ export class ResolverHostWithCache extends AbstractResolverHost {
       // Make sure we don't get an uncaught rejection
       inflight
         .then(result => {
+          cache.set(href, result);
+
           if (idb) {
             idb.put(storeName, serialize ? serialize(result) : result, href).catch(_ => undefined);
           }
@@ -90,6 +108,7 @@ export class ResolverHostWithCache extends AbstractResolverHost {
     const result = await this.withCache(
       url.href,
       () => this.host.getCanonicalUrl(resolver, url),
+      this.getCanonicalUrlCache,
       this.inflightGetCanonicalUrl,
       'getCanonicalUrl',
       url => url.href,
@@ -103,6 +122,7 @@ export class ResolverHostWithCache extends AbstractResolverHost {
     const result = await this.withCache(
       url.href,
       () => this.host.getResolveRoot(resolver, url),
+      this.getResolveRootCache,
       this.inflightGetResolveRoot,
       'getResolveRoot',
       url => url.href,
@@ -116,6 +136,7 @@ export class ResolverHostWithCache extends AbstractResolverHost {
     const result = await this.withCache(
       url.href,
       () => this.host.listEntries(resolver, url),
+      this.listEntriesCache,
       this.inflightListEntries,
       'listEntries',
       entries =>
@@ -138,6 +159,7 @@ export class ResolverHostWithCache extends AbstractResolverHost {
       const result = await this.withCache(
         url.href,
         () => this.host.readFileContent(resolver, url),
+        this.readFileContentCache,
         this.inflightReadFileContent,
         'readFileContent'
       );
