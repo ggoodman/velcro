4cb18b99b7798690376ba1a3ed59b3b2068daeda
Geoff Goodman <ggoodman@gmail.com>
2020-05-15 16:26:18 -0400

    Fix base64

--- packages/@velcro/common/src/base64.ts
+++ packages/@velcro/common/src/base64.ts
@@ -1,3 +1,5 @@
+import { atob, btoa } from 'b2a';
+
 declare const Buffer: {
   from(
     buf: BufferSource | string,
@@ -9,24 +11,10 @@ declare const Buffer: {
 
 export namespace Base64 {
   export const decode =
-    typeof atob === 'function'
-      ? (data: string) => atob(data)
-      : typeof Buffer !== 'undefined'
+    typeof Buffer !== 'undefined'
       ? (data: string) => Buffer.from(data, 'base64').toString('utf-8')
-      : () => {
-          throw new Error(
-            `The environment provides neither the atob function nor the Buffer API. Please consider polyfilling one of these.`
-          );
-        };
+      : atob;
 
   export const encode =
-    typeof btoa === 'function'
-      ? (data: string) => btoa(data)
-      : typeof Buffer !== 'undefined'
-      ? (data: string) => Buffer.from(data).toString('base64')
-      : () => {
-          throw new Error(
-            `The environment provides neither the btoa function nor the Buffer API. Please consider polyfilling one of these.`
-          );
-        };
+    typeof Buffer !== 'undefined' ? (data: string) => Buffer.from(data).toString('base64') : btoa;
 }
