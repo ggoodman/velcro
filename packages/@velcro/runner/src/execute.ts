2fba6bef9c2ccd7fa82ffee45cf23b3dc3a13eff
Geoff Goodman <ggoodman@gmail.com>
2020-05-19 21:02:32 -0400

    runner: Split into execute and build

--- packages/@velcro/runner/src/execute.ts
+++ packages/@velcro/runner/src/execute.ts
@@ -7,19 +7,22 @@ import { MemoryStrategy } from '@velcro/strategy-memory';
 const defaultExtensions: Resolver.Settings['extensions'] = ['.js', '.json'];
 const defaultPackageMain: Resolver.Settings['packageMain'] = ['browser', 'main'];
 
-export interface ExecuteOptions {
+export interface BuildOptions {
   cdn?: 'jsdelivr' | 'unpkg';
   dependencies?: { [key: string]: string };
   extensions?: Resolver.Settings['extensions'];
   external?: BuildGraphOptions['external'];
-  injectModules?: { [id: string]: unknown };
   nodeEnv?: string;
   packageMain?: Resolver.Settings['packageMain'];
   readUrl: CdnStrategy.UrlContentFetcher;
   sourceMap?: boolean;
 }
 
-export async function execute(code: string, options: ExecuteOptions) {
+export interface ExecuteOptions extends BuildOptions {
+  injectModules?: { [id: string]: unknown };
+}
+
+export async function build(code: string, options: BuildOptions) {
   const entrypointPath = 'index.js';
   const cdnStrategy =
     options.cdn === 'unpkg'
@@ -47,12 +50,18 @@ export async function execute(code: string, options: ExecuteOptions) {
     nodeEnv: options.nodeEnv || 'development',
   });
   const [chunk] = graph.splitChunks();
-  const build = chunk.buildForStaticRuntime({
+  const output = chunk.buildForStaticRuntime({
     injectRuntime: true,
   });
-  const codeWithStart = `${build.code}\n\nreturn Velcro.runtime;\n`;
+
+  return { entrypointUri, output };
+}
+
+export async function execute<T = unknown>(code: string, options: ExecuteOptions) {
+  const { entrypointUri, output } = await build(code, options);
+  const codeWithStart = `${output.code}\n\nreturn Velcro.runtime;\n`;
   const runtimeCode = options.sourceMap
-    ? `${codeWithStart}\n//# sourceMappingURL=${build.sourceMapDataUri}`
+    ? `${codeWithStart}\n//# sourceMappingURL=${output.sourceMapDataUri}`
     : codeWithStart;
   const runtimeFn = new Function(runtimeCode) as () => VelcroRuntime;
   const velcro = runtimeFn();
@@ -65,5 +74,5 @@ export async function execute(code: string, options: ExecuteOptions) {
 
   const result = velcro.require(entrypointUri.toString());
 
-  return result;
+  return result as T;
 }
