a81de9cfaca20abd1e9974d56efbc34cd439d8e4
Geoff Goodman <ggoodman@gmail.com>
2020-05-20 12:17:46 -0400

    Fix circular module instantiation in runtime

--- __test__/runner.ts
+++ __test__/runner.ts
@@ -39,7 +39,32 @@ describe('Velcro.runner', () => {
       nodeEnv: 'production',
     });
 
-    expect(result).toEqual('<h1 data-reactroot="">hello world</h1>');
+    expect(result).toMatchInlineSnapshot(`"<h1 data-reactroot=\\"\\">hello world</h1>"`);
+  });
+
+  it('will run more complex package using react-dom/server', async () => {
+    const code = `
+      const React = require('react');
+      const ReactDOMServer = require('react-dom/server');
+      const Carousel = require('react-alice-carousel').default;
+
+      const comp = React.createElement(Carousel, { items: [] });
+
+      module.exports = ReactDOMServer.renderToString(comp);
+    `;
+    const result = await execute(code, {
+      dependencies: {
+        react: '^16.13.1',
+        'react-dom': '^16.13.1',
+        'react-alice-carousel': '1.18.0',
+      },
+      readUrl,
+      nodeEnv: 'production',
+    });
+
+    expect(result).toMatchInlineSnapshot(
+      `"<div class=\\"alice-carousel\\" data-reactroot=\\"\\"><div><div style=\\"height:;transition:;padding-left:0px;padding-right:0px\\" class=\\"alice-carousel__wrapper\\"><ul style=\\"transition:transform 0ms;transform:translate3d(0px, 0, 0)\\" class=\\"alice-carousel__stage\\"></ul></div></div><ul class=\\"alice-carousel__dots\\"></ul><div class=\\"alice-carousel__prev-btn\\"><div class=\\"alice-carousel__prev-btn-wrapper\\"><p class=\\"alice-carousel__prev-btn-item\\"><span data-area=\\"prev\\"></span></p></div></div><div class=\\"alice-carousel__next-btn\\"><div class=\\"alice-carousel__next-btn-wrapper\\"><p class=\\"alice-carousel__next-btn-item\\"><span data-area=\\"next\\"></span></p></div></div></div>"`
+    );
   });
 
   it('will require the events shim', async () => {
