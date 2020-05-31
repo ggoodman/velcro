b877d44262aadf122732c34afe8820a87564dd45
Geoff Goodman <ggoodman@gmail.com>
2020-05-30 22:20:19 -0400

    More global fixes

--- packages/@velcro/node-libs/lib/global.js
+++ packages/@velcro/node-libs/lib/global.js
@@ -1,14 +1,13 @@
+/* global __magic__ globalThis */
 'use strict';
 
 module.exports = (function () {
-  if (typeof globalThis === 'object') return;
+  if (typeof globalThis === 'object') return globalThis;
   Object.prototype.__defineGetter__('__magic__', function () {
     return this;
   });
-  // eslint-disable-next-line no-undef
   __magic__.globalThis = __magic__;
   delete Object.prototype.__magic__;
 
-  // eslint-disable-next-line no-undef
   return globalThis;
 })();
