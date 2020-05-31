efe5fdbcf7dd6f803c507211b6a750eb3df500a8
Geoff Goodman <ggoodman@gmail.com>
2020-05-30 21:24:32 -0400

    Tweak globalThis polyfill

--- packages/@velcro/node-libs/lib/global.js
+++ packages/@velcro/node-libs/lib/global.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = require('globalthis/polyfill')();
+module.exports = (function () {
+  if (typeof globalThis === 'object') return;
+  Object.prototype.__defineGetter__('__magic__', function () {
+    return this;
+  });
+  // eslint-disable-next-line no-undef
+  __magic__.globalThis = __magic__;
+  delete Object.prototype.__magic__;
+})();
