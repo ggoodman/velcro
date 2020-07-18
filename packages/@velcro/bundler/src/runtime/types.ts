a751605c1ec2b68dcd0dfb34e3c5d7b08a309668
Geoff Goodman <ggoodman@gmail.com>
2020-07-17 21:25:23 -0400

    Lay groundwork for generational modules

--- packages/@velcro/bundler/src/runtime/types.ts
+++ packages/@velcro/bundler/src/runtime/types.ts
@@ -4,7 +4,9 @@ export type VelcroImportMap = {
   imports?: { [key: string]: string };
   scopes?: { [fromId: string]: { [key: string]: string | undefined } | undefined };
 };
+
 export type VelcroModule = { exports: Record<string | number | symbol, unknown> };
+
 export type VelcroModuleFactory = (
   module: VelcroModule,
   exports: VelcroModule['exports'],
@@ -12,18 +14,17 @@ export type VelcroModuleFactory = (
   __dirname: string,
   __filename: string
 ) => void;
-export type VelcroModuleLinks = {
-  [key: string]: { name: string; spec: string; path: string } | undefined;
-};
-export type VelcroModuleIdentification = { name: string; version: string; path: string };
+
 export interface VelcroRequire {
   (spec: string): unknown;
   resolve(spec: string): string;
 }
 
+type VelcroModuleGeneration = number;
+
 export type VelcroStaticRuntime = {
   defs: {
-    [key: string]: [VelcroModuleFactory, VelcroImportMap] | undefined;
+    [key: string]: [VelcroModuleFactory, VelcroImportMap, VelcroModuleGeneration] | undefined;
   };
   runtime?: VelcroRuntime;
 };
