bb4e5641c93e88612c42362a3e54fef92489fa03
Geoff Goodman <ggoodman@gmail.com>
2020-10-23 09:30:43 -0400

    Update http recordings

--- jest.config.js
+++ jest.config.js
@@ -106,7 +106,6 @@ module.exports = {
       testMatch: ['<rootDir>/__test__/**/*.ts'],
       globals: {
         pollyConfig: {
-          mode: MODES.REPLAY,
           // Recording missing requests is (unfortunately) required during the transition
           // to a new version (such as CI) where references to `@velcro/node-libs@<new_version>`
           // will be made that will need to be recorded and allowed.
@@ -143,7 +142,6 @@ module.exports = {
       testMatch: [`<rootDir>/packages/${name}/**/*.test.ts`],
       globals: {
         pollyConfig: {
-          mode: MODES.REPLAY,
           // Recording missing requests is (unfortunately) required during the transition
           // to a new version (such as CI) where references to `@velcro/node-libs@<new_version>`
           // will be made that will need to be recorded and allowed.
